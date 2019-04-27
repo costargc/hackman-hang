b1620fe4591dfb526844909dfdfa897b2e54824a
Rodrigo Costa <costa.rgc@gmail.com>
2019-04-27 10:22:32 -0400

    hang

---
+++ app.js
@@ -0,0 +1,2 @@
+var allpass=["123123","123456","654321","!@#$%^&*","1qaz2wsx","aa123456","abc123","access","admin","ashley","azerty","bailey","baseball","batman","charlie","donald","dragon","flower","Football","freedom","hello","hottie","iloveyou","jesus","letmein","login","loveme","master","michael","monkey","mustang","ninja","passw0rd","password","password1","princess","qazwsx","qwerty","qwerty123","qwertyuiop","shadow","solo","starwars","sunshine","superman","trustno1","welcome","whatever","zaq1zaq1","hangman","costa"]
+console.log(allpass.length);
