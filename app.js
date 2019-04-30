5f89abdabca2622cc418d2762a57962e3cb6ff10
Rodrigo Costa <costa.rgc@gmail.com>
2019-04-30 11:48:44 -0400

    v4.1

--- app.js
+++ app.js
@@ -36,11 +36,59 @@ document.getElementById("pass_hidden").textContent = secret.join('|');
 for (var i = 0; i < allowedchars.length; i++) {
     var button = document.createElement("button");
     button.textContent = allowedchars[i];
-    button.setAttribute('onclick', 'clickme("' + allowedchars[i] + '")');
     button.setAttribute("class","letter");
-    button.id = "letter_" + allowedchars[i];
+    button.setAttribute("id","letter_" + allowedchars[i]);
     document.getElementById('buttons').appendChild(button);
+    button.setAttribute('onclick', 'clickme("' + allowedchars[i] + '")');
+    // document.getElementById("letter_"+allowedchars[i]).onclick = function(){ clickme(allowedchars[i])};
 }
+
+//bad implementation... but don't need to use "onclick in html anymore"
+document.getElementById("playAgain").onclick = function(){ playAgain()};
+
+// document.getElementById("letter_a").onclick = function(){ clickme("a")};
+// document.getElementById("letter_b").onclick = function(){ clickme("b")};
+// document.getElementById("letter_c").onclick = function(){ clickme("c")};
+// document.getElementById("letter_d").onclick = function(){ clickme("d")};
+// document.getElementById("letter_e").onclick = function(){ clickme("e")};
+// document.getElementById("letter_f").onclick = function(){ clickme("f")};
+// document.getElementById("letter_g").onclick = function(){ clickme("g")};
+// document.getElementById("letter_h").onclick = function(){ clickme("h")};
+// document.getElementById("letter_i").onclick = function(){ clickme("i")};
+// document.getElementById("letter_j").onclick = function(){ clickme("j")};
+// document.getElementById("letter_k").onclick = function(){ clickme("k")};
+// document.getElementById("letter_l").onclick = function(){ clickme("l")};
+// document.getElementById("letter_m").onclick = function(){ clickme("m")};
+// document.getElementById("letter_n").onclick = function(){ clickme("n")};
+// document.getElementById("letter_o").onclick = function(){ clickme("o")};
+// document.getElementById("letter_p").onclick = function(){ clickme("p")};
+// document.getElementById("letter_q").onclick = function(){ clickme("q")};
+// document.getElementById("letter_r").onclick = function(){ clickme("r")};
+// document.getElementById("letter_s").onclick = function(){ clickme("s")};
+// document.getElementById("letter_t").onclick = function(){ clickme("t")};
+// document.getElementById("letter_u").onclick = function(){ clickme("u")};
+// document.getElementById("letter_w").onclick = function(){ clickme("w")};
+// document.getElementById("letter_v").onclick = function(){ clickme("v")};
+// document.getElementById("letter_x").onclick = function(){ clickme("x")};
+// document.getElementById("letter_y").onclick = function(){ clickme("y")};
+// document.getElementById("letter_z").onclick = function(){ clickme("z")};
+// document.getElementById("letter_1").onclick = function(){ clickme("1")};
+// document.getElementById("letter_2").onclick = function(){ clickme("2")};
+// document.getElementById("letter_3").onclick = function(){ clickme("3")};
+// document.getElementById("letter_4").onclick = function(){ clickme("4")};
+// document.getElementById("letter_5").onclick = function(){ clickme("5")};
+// document.getElementById("letter_6").onclick = function(){ clickme("6")};
+// document.getElementById("letter_7").onclick = function(){ clickme("7")};
+// document.getElementById("letter_8").onclick = function(){ clickme("8")};
+// document.getElementById("letter_9").onclick = function(){ clickme("9")};
+// document.getElementById("letter_0").onclick = function(){ clickme("0")};
+// document.getElementById("letter_!").onclick = function(){ clickme("!")};
+// document.getElementById("letter_@").onclick = function(){ clickme("@")};
+// document.getElementById("letter_#").onclick = function(){ clickme("#")};
+// document.getElementById("letter_$").onclick = function(){ clickme("$")};
+// document.getElementById("letter_%").onclick = function(){ clickme("%")};
+// document.getElementById("letter_&").onclick = function(){ clickme("&")};
+// document.getElementById("letter_?").onclick = function(){ clickme("?")};
 
 
 // Next, we give JavaScript a function to execute when onkeyup event fires.
@@ -90,7 +138,7 @@ function clickme(userText) {
 
     }
     //I need to add a logic to disable a button
-    document.getElementById('letter_' + userText).setAttribute("disabled", 'disabled');
+    document.getElementById('letter_' + userText).setAttribute("disabled", "disabled");
 
     //check if win or lose
     if (secret.indexOf("*") == -1 && lives > 0) {
