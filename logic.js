ad5e94527d3ab3e96d35cc1ddb54a9254a0ae241
Rodrigo Costa <costa.rgc@gmail.com>
2019-05-03 20:37:27 -0400

    v7.0

--- logic.js
+++ logic.js
@@ -255,19 +255,19 @@ function glowMe(status) {
     if (status == "losses") {
         // glow effect win
         document.getElementById("title_l").setAttribute("id", "title_l_glow");
-        document.getElementById("scoreboard_l").setAttribute("id", "scoreboard_l_glow");
+        document.getElementById("loss-counter").setAttribute("id", "loss-counter_glow");
         setTimeout(function () {
             document.getElementById("title_l_glow").setAttribute("id", "title_l");
-            document.getElementById("scoreboard_l_glow").setAttribute("id", "scoreboard_l");
+            document.getElementById("loss-counter_glow").setAttribute("id", "loss-counter");
         }, 700);
     }
     else if (status == "wins") {
         // glow effect win
         document.getElementById("title_w").setAttribute("id", "title_w_glow");
-        document.getElementById("scoreboard_w").setAttribute("id", "scoreboard_w_glow");
+        document.getElementById("win-counter").setAttribute("id", "win-counter_glow");
         setTimeout(function () {
             document.getElementById("title_w_glow").setAttribute("id", "title_w");
-            document.getElementById("scoreboard_w_glow").setAttribute("id", "scoreboard_w");
+            document.getElementById("win-counter_glow").setAttribute("id", "win-counter");
         }, 700);
     }
 }
